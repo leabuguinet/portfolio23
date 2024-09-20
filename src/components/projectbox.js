8000b04a1dd348f990f2f9abf2628ff366497d5f
Léa Buguinet <lea.buguinet@gmail.com>
2024-09-20 18:20:12 +0200

    portfolio box anim

--- src/components/projectbox.js
+++ src/components/projectbox.js
@@ -1,80 +1,61 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
 
-
-import { motion } from "framer-motion";
-
+import { motion, AnimatePresence } from "framer-motion";
 
 import dataProject from "../assets/data/data-project.json";
-import Fleche from '../assets/svg/fleche.inline.svg';
-import Fleche2 from '../assets/svg/fleche.inline.svg';
+import Fleche from "../assets/svg/fleche.inline.svg";
+import Fleche2 from "../assets/svg/fleche.inline.svg";
 
-export default function ProjectBox ({setShowProject, index, scrollPosition}){
-
-
+export default function ProjectBox({
+  setShowProject,
+  index,
+  scrollPosition,
+  showProject,
+}) {
   function closeModal() {
     setShowProject(false);
-    let body = document.querySelector('body');
-    body.classList.remove('overflow')
-}
+    let body = document.querySelector("body");
+    body.classList.remove("overflow");
+  }
 
-const test = {
-  hidden: { y: 200, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 0.51,
-  },
-};
   return (
-   
-    <motion.div
-                
-                  className="project-overlay"
-                  variants={test}
-                  style={{ top: scrollPosition }}
-                  onClick={() => closeModal()  }
-                >
 
-      <div className="project-box" /* onClick={(e) => e.stopPropagation()} */  >
-
+    <div className="project-box">
       <div className="project-content">
-          <h2>{dataProject[index].title}</h2>
-          <div className="close-modal-button"
-                onClick={() => closeModal()  }
-                
-              >
-          
-              </div>
-        </div>
-        <div className="project-content">
-
-          <div className="project-description">
-
-            <div>
+        <h2>{dataProject[index].title}</h2>
+        <div
+          className="close-modal-button"
+           onClick={() => closeModal()}
+        ></div>
+      </div>
+      <div className="project-content">
+        <div className="project-description">
+          <div>
             <p>{dataProject[index].description}</p>
 
             <p>Technologies : {dataProject[index].technologies}</p>
 
             <p>{dataProject[index].year}</p>
-
-            </div>
-            <div>
-            <a className="button" target="_blank" href={dataProject[index].link}>
-            <span className="button__icon-wrapper">
+          </div>
+          <div>
+            <a
+              className="button"
+              target="_blank"
+              href={dataProject[index].link}
+            >
+              <span className="button__icon-wrapper">
                 <Fleche />
 
-                <Fleche2 className="button__icon-svg--copy"/>
-            </span>
-            Voir le site
-          </a>
+                <Fleche2 className="button__icon-svg--copy" />
+              </span>
+              Voir le site
+            </a>
           </div>
-
-          </div>
-            <img src={'' + dataProject[index].img}/>
         </div>
-
-
+        <img src={"" + dataProject[index].img} />
       </div>
-      </motion.div>
+    </div>
+  /* </div> */
   );
-};
+}
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -6,7 +6,7 @@ import Nav from "../components/nav";
 
 import ProjectBox from "../components/projectbox";
 
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 import Project1 from "../assets/jpg/karabafc.png";
 import Project2 from "../assets/jpg/ajmariage.png";
@@ -23,18 +23,26 @@ const numbers = [Project1, Project2, Project3, Project4, Project5, Project6];
 let indexproject;
 
 const Portfolio = ({ location }) => {
-
   const pageUrl = location.pathname;
   const [showProject, setShowProject] = useState(false);
 
   function openModal(index) {
     indexproject = index;
     setShowProject(!showProject);
-    let body = document.querySelector('body');
-    body.classList.add('overflow')
-}
+    let body = document.querySelector("body");
+    body.classList.add("overflow");
+  }
+
+  function closeModal() {
+    setShowProject(false);
+    let body = document.querySelector("body");
+    body.classList.remove("overflow");
+  }
 
 
+  const toggleVisibility = () => {
+    setShowProject(!showProject);
+  };
 
   const container = {
     hidden: { opacity: 0 },
@@ -47,8 +55,6 @@ const Portfolio = ({ location }) => {
     },
   };
 
-
-
   const item = {
     hidden: { y: 20, opacity: 0 },
     visible: {
@@ -60,17 +66,16 @@ const Portfolio = ({ location }) => {
 
   const [scrollPosition, setScrollPosition] = useState(0);
   const handleScroll = () => {
-      const position = window.scrollY;
-      setScrollPosition(position);
-
+    const position = window.scrollY;
+    setScrollPosition(position);
   };
-  
-  useEffect(() => {
-      window.addEventListener('scroll', handleScroll, { passive: true });
 
-      return () => {
-          window.removeEventListener('scroll', handleScroll);
-      };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   return (
@@ -89,14 +94,12 @@ const Portfolio = ({ location }) => {
             >
               {[0, 1, 2, 3, 4, 5].map((index) => (
                 <motion.button
-                
                   key={index}
                   className="gallery-item"
                   variants={item}
                   style={{ originX: 0, originZ: 0 }}
                   onClick={() => openModal(index)}
                   index={index}
-
                 >
                   <motion.img
                     variants={item}
@@ -105,14 +108,33 @@ const Portfolio = ({ location }) => {
                   ></motion.img>
                 </motion.button>
               ))}
-
-              
             </motion.div>
-            
           </div>
         </div>
-        {showProject && <ProjectBox setShowProject={setShowProject} index={indexproject} scrollPosition={scrollPosition}/>}
 
+
+        <AnimatePresence>
+        {showProject && (
+          <motion.div
+          className="project-overlay"
+            key="modal"
+            initial={{ opacity: 0, top: scrollPosition }}
+            animate={{ opacity: 1, top: scrollPosition}}
+            exit={{ opacity: 0 }}
+            style={{top: scrollPosition}}
+            onClick={() => closeModal()}
+          >
+            <ProjectBox          showProject={setShowProject}
+          setShowProject={setShowProject}
+          index={indexproject}
+          scrollPosition={scrollPosition}  ></ProjectBox>
+            </motion.div>
+        )}
+      </AnimatePresence>
+
+     
+     
+    
       </Layout>
     </>
   );
